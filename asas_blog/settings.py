4580a8d87a76dabaa16b1cd280a6f5ab7b959678
Elgiwawee <uz3443442@gmail.com>
2025-02-26 17:49:57 +0100

    Update

--- asas_blog/settings.py
+++ asas_blog/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 'django-insecure-0%&4v05u-%)1m&e4in^!gs$6+ot&@#ls5r9=iy9dueke2%o(94
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
